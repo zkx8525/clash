9b0478c6961ef3a0a3c4e10b016acd24f28fda8b
Speh Ruza <109951664+zkx8525@users.noreply.github.com>
2023-11-16 21:38:44 +0800

    Update route.sh

--- route.sh
+++ route.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# 检查是否有 root 权限
-if [[ $EUID -ne 0 ]]; then
-   echo "请使用 root 权限运行此脚本" 
-   exit 1
-fi
-
-# 定义配置文件路径
+# 设置配置文件路径
 config_file="/etc/XrayR/config.yml"
-route_json_url="https://raw.githubusercontent.com/zkx8525/clash/main/route.json"
-route_json_file="/etc/XrayR/route.json"
 
-# 备份原始配置文件
-cp "$config_file" "$config_file.backup"
+# 修改配置文件中的路径
+sed -i 's|# /etc/XrayR/route.json|/etc/XrayR/route.json|g' "$config_file"
+sed -i 's|# /etc/XrayR/custom_outbound.json|/etc/XrayR/custom_outbound.json|g' "$config_file"
 
-# 修改配置文件
-sed -i 's|#/etc/XrayR/route.json|/etc/XrayR/route.json|g' "$config_file"
-sed -i 's|#/etc/XrayR/custom_outbound.json|/etc/XrayR/custom_outbound.json|g' "$config_file"
+# 下载新的route.json文件
+curl -o /etc/XrayR/route.json https://raw.githubusercontent.com/zkx8525/clash/main/route.json
 
-# 下载新的 route.json 文件
-wget -O "$route_json_file" "$route_json_url"
-
-# 重新运行 xrayr
-systemctl restart xrayr
-
-echo "配置文件已更新。原始配置文件备份为 $config_file.backup"
+echo "配置文件已更新。"
+XrayR restart
